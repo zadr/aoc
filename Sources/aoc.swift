c455d7a9c35f44983531af7c160c34ec7ffe9232
Zachary Drayer <zacharydrayer@gmail.com>
2023-12-08 23:26:03 -0800

    add --begin flag to set the next day up

--- Sources/aoc.swift
+++ Sources/aoc.swift
@@ -11,6 +11,9 @@ struct AOC: ParsableCommand {
 	@Option(help: "the year to run. defaults to the latest year when numerically sorted.")
 	var year: Int? = nil
 
+	@Flag(help: "set up files for the next day. may be combined with --day and --year.")
+	var begin = false
+
 	@Flag(help: "use real data for testing? set to true when ready.")
 	var realData = false
 
@@ -35,6 +38,14 @@ struct AOC: ParsableCommand {
 		let yearToRun = year ?? solutions.keys.sorted().last!
 		let dayToRun = day ?? solutions[yearToRun]!.keys.sorted().last!
 
+		if begin {
+			prepare(dayToPrepare: dayToRun + 1, yearToPrepare: yearToRun)
+		} else {
+			run(solutions, dayToRun: dayToRun, yearToRun: yearToRun)
+		}
+	}
+
+	func run(_ solutions: [Int: [Int: Solution]], dayToRun: Int, yearToRun: Int) {
 		let clock = ContinuousClock()
 
 		print("Running \(yearToRun) day \(dayToRun)\n")
@@ -59,5 +70,42 @@ struct AOC: ParsableCommand {
 			print(duration.formatted())
 		}
 	}
+
+	func prepare(dayToPrepare: Int, yearToPrepare: Int) {
+		let solution = """
+import Foundation
+
+class AOC_\(yearToPrepare)_\(dayToPrepare): NSObject, Solution {
+	static func solver() -> Solution { AOC_\(yearToPrepare)_\(dayToPrepare)() }
+
+	func partOne(useSampleData: Bool) -> Output {
+		_ = (useSampleData ? AOC_\(yearToPrepare)_\(dayToPrepare).sample : AOC_\(yearToPrepare)_\(dayToPrepare).real)
+		return .int(0)
+	}
+
+	func partTwo(useSampleData: Bool) -> Output {
+		_ = (useSampleData ? AOC_\(yearToPrepare)_\(dayToPrepare).sample : AOC_\(yearToPrepare)_\(dayToPrepare).real)
+		return .int(0)
+	}
 }
 
+"""
+		let solutionPath = "Sources/\(yearToPrepare)/\(yearToPrepare)_\(dayToPrepare).swift"
+		try! solution.write(to: URL(fileURLWithPath: solutionPath), atomically: true, encoding: .utf8)
+
+		let tripleQuotes = "\"\"\""
+		let input = """
+extension AOC_\(yearToPrepare)_\(dayToPrepare) {
+	static let sample = \(tripleQuotes)
+\(tripleQuotes)
+
+	static let real = \(tripleQuotes)
+\(tripleQuotes)
+}
+
+"""
+
+		let inputPath = "Sources/\(yearToPrepare)/\(yearToPrepare)_\(dayToPrepare)+Data.swift"
+		try! input.write(to: URL(fileURLWithPath: inputPath), atomically: true, encoding: .utf8)
+	}
+}
